0d2bdf07e74b547cacbc38fe14252cf813dcd06b
KimDoubleB <tree9295@gmail.com>
2021-10-23 16:00:50 +0900

    refactor: specify generation type at ArchiveImage

--- src/main/java/com/depromeet/archive/domain/archive/entity/ArchiveImage.java
+++ src/main/java/com/depromeet/archive/domain/archive/entity/ArchiveImage.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -18,7 +19,7 @@ import javax.persistence.Table;
 public class ArchiveImage extends BaseTimeEntity {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "archive_image_id")
     private Long id;
 
