fcf0bcb6f884f0150bdfd495ad91b94ac291dea4
juseok.kim <juseok.kim@toss.im>
2021-10-28 23:25:29 +0900

    [#23] 컴파일 오류 수정

--- src/main/java/com/depromeet/archive/security/oauth/converter/KakaoPrincipalConverter.java
+++ src/main/java/com/depromeet/archive/security/oauth/converter/KakaoPrincipalConverter.java
@@ -2,6 +2,7 @@ package com.depromeet.archive.security.oauth.converter;
 
 
 import com.depromeet.archive.domain.user.entity.UserRole;
+import com.depromeet.archive.domain.user.info.UserInfo;
 import com.depromeet.archive.security.oauth.UserPrincipalConverter;
 import com.depromeet.archive.security.common.UserPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
@@ -19,10 +20,13 @@ public class KakaoPrincipalConverter implements UserPrincipalConverter {
     public UserPrincipal convert(OAuth2User oAuth2User) {
         Map<String, Object> attributes = ((Map) oAuth2User.getAttribute("kakao_account"));
         String email = (String) attributes.get("email");
-        return UserPrincipal
-                .builder()
+        UserInfo info = UserInfo.builder()
                 .mailAddress(email)
                 .userRole(UserRole.GENERAL)
+                .build();
+        return UserPrincipal
+                .builder()
+                .userInfo(info)
                 .attributes(attributes)
                 .build();
     }
