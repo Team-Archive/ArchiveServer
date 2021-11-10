2df0900afefe564936e42479641b3ac65d34932b
KimDoubleB <tree9295@gmail.com>
2021-11-10 22:50:37 +0900

    refactor: update request mapping (use api/v1)

--- src/main/java/com/depromeet/archive/controller/ArchiveController.java
+++ src/main/java/com/depromeet/archive/controller/ArchiveController.java
@@ -7,20 +7,22 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/archive")
 @RequiredArgsConstructor
 public class ArchiveController {
 
     private final ArchiveService archiveService;
 
-    @GetMapping("/archive")
+    @GetMapping
     public ResponseEntity<ArchiveListDto> archiveListView() {
         return ResponseEntity.ok(archiveService.getAllArchive());
     }
 
-    @GetMapping("/archive/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<ArchiveDto> archiveSpecificView(@PathVariable Long id) {
         return ResponseEntity.ok(archiveService.getOneArchiveById(id));
     }
