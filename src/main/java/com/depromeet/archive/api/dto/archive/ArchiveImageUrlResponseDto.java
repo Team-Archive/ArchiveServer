59b543a2bb42b4f5e99fcb0525ece61a0d7ddf55
KimDoubleB <tree9295@gmail.com>
2021-11-13 12:47:18 +0900

    feat: added image upload api to ArchiveController

---
+++ src/main/java/com/depromeet/archive/api/dto/archive/ArchiveImageUrlResponseDto.java
@@ -0,0 +1,12 @@
+package com.depromeet.archive.api.dto.archive;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class ArchiveImageUrlResponseDto {
+
+    private final String imageUrl;
+
+}
