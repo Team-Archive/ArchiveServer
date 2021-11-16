8c73255a8f805625cdc1767298d5f78f8e4a21e9
junsekim <junsekim@student.42seoul.kr>
2021-11-16 20:51:39 +0900

    [#23] 컴파일 오류 해결

--- src/main/java/com/depromeet/archive/config/WebConfigurer.java
+++ src/main/java/com/depromeet/archive/config/WebConfigurer.java
@@ -1,6 +1,6 @@
 package com.depromeet.archive.config;
 
-import com.depromeet.archive.controller.resolver.UserArgumentResolver;
+import com.depromeet.archive.api.resolver.UserArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
