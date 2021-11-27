d891193459a64eb50fd0f32d2f23952cc8d8a86c
KimDoubleB <tree9295@gmail.com>
2021-11-27 14:57:06 +0900

    feat: added swagger config

--- src/main/java/com/depromeet/archive/api/ArchiveController.java
+++ src/main/java/com/depromeet/archive/api/ArchiveController.java
@@ -5,6 +5,7 @@ import com.depromeet.archive.api.dto.archive.ArchiveImageUrlResponseDto;
 import com.depromeet.archive.api.dto.archive.ArchiveListDto;
 import com.depromeet.archive.domain.archive.ArchiveImageService;
 import com.depromeet.archive.domain.archive.ArchiveService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -27,21 +28,25 @@ public class ArchiveController {
     private final ArchiveService archiveService;
     private final ArchiveImageService imageService;
 
+    @Operation(summary = "아카이브 리스트 조회", description = "홈 뷰 - 아카이브 리스트 조회")
     @GetMapping
     public ResponseEntity<ArchiveListDto> archiveListView() {
         return ResponseEntity.ok(archiveService.getAllArchive());
     }
 
+    @Operation(summary = "아카이브 상세 조회", description = "상세 뷰 - 아카이브 상세 조회")
     @GetMapping("/{id}")
     public ResponseEntity<ArchiveDto> archiveSpecificView(@PathVariable Long id) {
         return ResponseEntity.ok(archiveService.getOneArchiveById(id));
     }
 
+    @Operation(summary = "아카이브 삭제", description = "아카이브 제거 - 실제 제거 X")
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id) {
         archiveService.delete(id);
     }
 
+    @Operation(summary = "이미지 업로드")
     @PostMapping(path = "/image/upload",
             consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
@@ -52,6 +57,7 @@ public class ArchiveController {
         return ResponseEntity.ok(new ArchiveImageUrlResponseDto(imageUrl));
     }
 
+    @Operation(summary = "아키이브 추가")
     @PostMapping
     public ResponseEntity<Object> addArchive(@RequestBody ArchiveDto archiveDto) {
         archiveService.save(archiveDto);
