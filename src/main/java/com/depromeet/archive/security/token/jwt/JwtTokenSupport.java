6fc06e967d44c9b47a07dab388fd41fcd4d65318
KimDoubleB <tree9295@gmail.com>
2021-12-11 14:37:05 +0900

    refactor: use AUTHORIZATION constant value of HttpHeaders

--- src/main/java/com/depromeet/archive/security/token/jwt/JwtTokenSupport.java
+++ src/main/java/com/depromeet/archive/security/token/jwt/JwtTokenSupport.java
@@ -3,6 +3,7 @@ package com.depromeet.archive.security.token.jwt;
 import com.depromeet.archive.exception.security.TokenNotFoundException;
 import com.depromeet.archive.security.token.HttpAuthTokenSupport;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpHeaders;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,12 +11,11 @@ import javax.servlet.http.HttpServletResponse;
 @Slf4j
 public class JwtTokenSupport implements HttpAuthTokenSupport {
 
-    public static final String AUTH_HEADER = "Authorization";
     public static final String TOKEN_TYPE = "BEARER";
 
     @Override
     public String extractToken(HttpServletRequest target) {
-        String tokenTypeAndStr = target.getHeader("Authorization");
+        String tokenTypeAndStr = target.getHeader(HttpHeaders.AUTHORIZATION);
         log.debug("Parsing token in header: {}", tokenTypeAndStr);
         if (isInvalidToken(tokenTypeAndStr))
             throw new TokenNotFoundException();
@@ -27,6 +27,6 @@ public class JwtTokenSupport implements HttpAuthTokenSupport {
     }
     @Override
     public void injectToken(HttpServletResponse dest, String token) {
-        dest.addHeader(AUTH_HEADER, TOKEN_TYPE + " " + token);
+        dest.addHeader(HttpHeaders.AUTHORIZATION, TOKEN_TYPE + " " + token);
     }
 }
