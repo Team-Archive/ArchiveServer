1eeacb88b6396315a32c1386d9b8cdb65330d4a9
IceMelon404 <kjs001127@naver.com>
2021-12-13 13:12:15 +0900

    feat: 특수문자 필수가 아니도록 수정

--- src/main/java/com/depromeet/archive/domain/user/command/CredentialRegisterCommand.java
+++ src/main/java/com/depromeet/archive/domain/user/command/CredentialRegisterCommand.java
@@ -15,7 +15,7 @@ import javax.validation.constraints.Pattern;
 public class CredentialRegisterCommand extends BasicRegisterCommand {
 
     @NotEmpty(message = "필수 입력 항목입니다.")
-    @Pattern(regexp = "^.*(?=^.{8,15}$)(?=.*\\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$",
+    @Pattern(regexp = "(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9$@$!%*#?&]{8,20}$",
             message = "비밀번호는 특수문자 / 문자 / 숫자 포함 형태의 8~15자리 이내로 입력해 주세요.")
     private String password;
 
