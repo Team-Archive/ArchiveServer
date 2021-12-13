d50b978b808b1aa0a74ae7b4c431833ee0ed79be
IceMelon404 <kjs001127@naver.com>
2021-12-13 13:14:13 +0900

    feat: 비밀번호 규칙 메세지 수정

--- src/main/java/com/depromeet/archive/domain/user/command/CredentialRegisterCommand.java
+++ src/main/java/com/depromeet/archive/domain/user/command/CredentialRegisterCommand.java
@@ -16,7 +16,7 @@ public class CredentialRegisterCommand extends BasicRegisterCommand {
 
     @NotEmpty(message = "필수 입력 항목입니다.")
     @Pattern(regexp = "(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9$@$!%*#?&]{8,20}$",
-            message = "비밀번호는 특수문자 / 문자 / 숫자 포함 형태의 8~15자리 이내로 입력해 주세요.")
+            message = "비밀번호는 영문/숫자 를 꼭 포함하여 8~20자리로 입력해 주세요.")
     private String password;
 
     public CredentialRegisterCommand(String userMail, String password) {
