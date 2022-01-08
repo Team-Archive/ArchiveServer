0a0c5e8004e5784305c4660ea858f3f0e2ad7c91
KimDoubleB <tree9295@gmail.com>
2022-01-08 15:56:37 +0900

    fix: remove readonly

--- src/main/java/com/depromeet/archive/domain/user/UserService.java
+++ src/main/java/com/depromeet/archive/domain/user/UserService.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional(readOnly = true)
+@Transactional
 @Slf4j
 @RequiredArgsConstructor
 public class UserService {
