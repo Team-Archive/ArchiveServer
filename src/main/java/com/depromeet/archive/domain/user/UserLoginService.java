dd305ea2ed295d2ac0dd9c671e3762f822f5ebf5
KimDoubleB <tree9295@gmail.com>
2022-04-12 22:45:39 +0900

    refactor: extract verify password method from tryLoginAndReturnInfo

--- src/main/java/com/depromeet/archive/domain/user/UserLoginService.java
+++ src/main/java/com/depromeet/archive/domain/user/UserLoginService.java
@@ -1,6 +1,7 @@
 package com.depromeet.archive.domain.user;
 
 import com.depromeet.archive.domain.user.command.LoginCommand;
+import com.depromeet.archive.domain.user.entity.PasswordUser;
 import com.depromeet.archive.domain.user.info.UserInfo;
 import com.depromeet.archive.exception.common.ResourceNotFoundException;
 import com.depromeet.archive.exception.user.LoginFailException;
@@ -19,11 +20,16 @@ public class UserLoginService {
     private final PasswordEncoder encoder;
 
     public UserInfo tryLoginAndReturnInfo(LoginCommand command) throws LoginFailException {
-        var user = repository.findPasswordUserByMailAddress(command.getEmail())
-                .orElseThrow(() -> new ResourceNotFoundException("Email"));
-        if (!encoder.matches(command.getPassword(), user.getPassword()))
-            throw new LoginFailException("로그인에 실패하였습니다. 비밀번호가 다릅니다.");
+        var user = verifyPasswordReturnUser(command.getEmail(), command.getPassword());
         return user.convertToUserInfo();
+    }
+
+    public PasswordUser verifyPasswordReturnUser(final String email, final String password) {
+        var user = repository.findPasswordUserByMailAddress(email)
+                .orElseThrow(() -> new ResourceNotFoundException("Email"));
+        if (!encoder.matches(password, user.getPassword()))
+            throw new LoginFailException("비밀번호가 다릅니다");
+        return user;
     }
 
 }
