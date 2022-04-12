819cfcaace09673069a95e87886702a973e11872
KimDoubleB <tree9295@gmail.com>
2022-04-12 20:49:27 +0900

    feat: update temporary password service logic

--- src/main/java/com/depromeet/archive/domain/user/UserService.java
+++ src/main/java/com/depromeet/archive/domain/user/UserService.java
@@ -5,12 +5,17 @@ import com.depromeet.archive.domain.common.MessagingService;
 import com.depromeet.archive.domain.user.command.BasicRegisterCommand;
 import com.depromeet.archive.domain.user.command.OAuthRegisterCommand;
 import com.depromeet.archive.domain.user.entity.BaseUser;
+import com.depromeet.archive.domain.user.entity.PasswordUser;
 import com.depromeet.archive.domain.user.info.UserInfo;
 import com.depromeet.archive.exception.common.DuplicateResourceException;
+import com.depromeet.archive.exception.common.ResourceNotFoundException;
+import com.depromeet.archive.exception.user.OAuthUserHasNotPasswordException;
+import com.depromeet.archive.infra.mail.MailService;
 import com.depromeet.archive.infra.user.jpa.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,6 +27,8 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final MessagingService messagingService;
+    private final PasswordEncoder encoder;
+    private final MailService mailService;
 
     public boolean existsEmail(String email) {
         return userRepository.findByMailAddress(email).isPresent();
@@ -49,6 +56,14 @@ public class UserService {
         }
     }
 
+    public void updateTemporaryPassword(final String email, final String temporaryPassword) {
+        var passwordUser = userRepository.findByMailAddress(email)
+                .map(this::convertPasswordUser)
+                .orElseThrow(() -> new ResourceNotFoundException("Email"));
+        passwordUser.setTemporaryPassword(encoder.encode(temporaryPassword));
+        mailService.sendTemporaryPassword(email, temporaryPassword);
+    }
+
     public void deleteUser(long userId) {
         BaseUser user = userRepository.getById(userId);
         userRepository.delete(user);
@@ -62,6 +77,13 @@ public class UserService {
         } else {
             messagingService.sendUserRegisterMessage(BaseUserDto.from(user), "Id/Password");
         }
+    }
+
+    private PasswordUser convertPasswordUser(BaseUser user) {
+        if (!(user instanceof PasswordUser)) {
+            throw new OAuthUserHasNotPasswordException();
+        }
+        return (PasswordUser) user;
     }
 
 }
