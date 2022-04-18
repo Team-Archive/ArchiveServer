da72b3efb6b2cfc798796cd913d3689dd24827c4
KimDoubleB <tree9295@gmail.com>
2022-04-18 20:37:40 +0900

    style: code reformat

--- src/main/java/com/depromeet/archive/security/result/LoginSuccessHandler.java
+++ src/main/java/com/depromeet/archive/security/result/LoginSuccessHandler.java
@@ -1,7 +1,5 @@
 package com.depromeet.archive.security.result;
 
-
-import com.depromeet.archive.domain.user.info.UserInfo;
 import com.depromeet.archive.security.common.UserPrincipal;
 import com.depromeet.archive.security.token.HttpAuthTokenSupport;
 import com.depromeet.archive.security.token.TokenProvider;
@@ -10,10 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Slf4j
 public class LoginSuccessHandler implements AuthenticationSuccessHandler {
@@ -21,17 +17,21 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler {
     private final TokenProvider provider;
     private final HttpAuthTokenSupport tokenSupport;
 
-    public LoginSuccessHandler(TokenProvider provider, HttpAuthTokenSupport tokenSupport) {
+    public LoginSuccessHandler(TokenProvider provider,
+                               HttpAuthTokenSupport tokenSupport) {
         this.provider = provider;
         this.tokenSupport = tokenSupport;
     }
 
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
-        UserPrincipal principal = (UserPrincipal) authentication.getPrincipal();
-        UserInfo authToken = principal.getUserInfo();
-        String successToken = provider.createToken(authToken);
+    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest,
+                                        HttpServletResponse httpServletResponse,
+                                        Authentication authentication) {
+        var principal = (UserPrincipal) authentication.getPrincipal();
+        var authToken = principal.getUserInfo();
+        var successToken = provider.createToken(authToken);
         log.debug("유저 로그인 성공, 아이디: {}, 이메일: {}, 토큰: {}", authToken.getUserId(), authToken.getMailAddress(), successToken);
+
         tokenSupport.injectToken(httpServletResponse, successToken);
         httpServletResponse.setStatus(HttpStatus.OK.value());
     }
