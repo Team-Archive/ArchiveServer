234113944752a83a2469a105af7fe10c49a663cb
KimDoubleB <tree9295@gmail.com>
2022-04-18 21:50:52 +0900

    feat: use DynamicInsert to PasswordUser

--- src/main/java/com/depromeet/archive/domain/user/entity/PasswordUser.java
+++ src/main/java/com/depromeet/archive/domain/user/entity/PasswordUser.java
@@ -2,20 +2,22 @@ package com.depromeet.archive.domain.user.entity;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
 
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@NoArgsConstructor
 @Entity
 @DiscriminatorValue("password")
-@NoArgsConstructor
-@Table(name= "password_user")
+@DynamicInsert
+@Table(name = "password_user")
 public class PasswordUser extends BaseUser {
 
     @Getter
-    @Column(name= "password")
+    @Column(name = "password")
     private String password;
 
     @Column(name = "is_temporary_password", columnDefinition = "boolean default false")
