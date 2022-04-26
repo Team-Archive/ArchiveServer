f13f029b5c45f0ba9ba0e595860072051e07f4a0
KimDoubleB <tree9295@gmail.com>
2022-04-26 21:29:55 +0900

    feat: implements AppleClient
    - get Email through parsing token

--- src/main/java/com/depromeet/archive/infra/user/oauth/provider/AppleClient.java
+++ src/main/java/com/depromeet/archive/infra/user/oauth/provider/AppleClient.java
@@ -3,8 +3,20 @@ package com.depromeet.archive.infra.user.oauth.provider;
 import com.depromeet.archive.api.dto.user.OAuthRegisterDto;
 import com.depromeet.archive.domain.user.command.OAuthRegisterCommand;
 import com.depromeet.archive.domain.user.entity.OAuthProvider;
+import com.depromeet.archive.exception.user.OAuthRegisterFailException;
+import com.depromeet.archive.infra.user.oauth.provider.dto.AppleTokenPayload;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nimbusds.jwt.SignedJWT;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
+import java.text.ParseException;
+
+@Component
+@RequiredArgsConstructor
 public class AppleClient implements OAuthProviderClient {
+
+    private final ObjectMapper objectMapper;
 
     @Override
     public String support() {
@@ -13,7 +25,13 @@ public class AppleClient implements OAuthProviderClient {
 
     @Override
     public OAuthRegisterCommand getOAuthRegisterInfo(OAuthRegisterDto oAuthRegisterDto) {
-        return null;
+        try {
+            var jwtClaimsSet = SignedJWT.parse(oAuthRegisterDto.getToken()).getJWTClaimsSet();
+            var payload = objectMapper.convertValue(jwtClaimsSet.toJSONObject(), AppleTokenPayload.class);
+            return new OAuthRegisterCommand(payload.getEmail(), OAuthProvider.APPLE);
+        } catch (ParseException e) {
+            throw new OAuthRegisterFailException(OAuthProvider.APPLE, "Apple token parse failure.");
+        }
     }
 
 }
