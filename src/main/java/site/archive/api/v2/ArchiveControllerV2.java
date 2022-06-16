ca19c3259336fe7426ecacf8c760051adcc6bbb4
KimDoubleB <tree9295@gmail.com>
2022-06-17 01:34:00 +0900

    refactor(#6): use request parameter instead of request body

--- src/main/java/site/archive/api/v2/ArchiveControllerV2.java
+++ src/main/java/site/archive/api/v2/ArchiveControllerV2.java
@@ -3,15 +3,13 @@ package site.archive.api.v2;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import site.archive.api.dto.archive.ArchiveDto;
 import site.archive.api.dto.archive.ArchiveListResponseDto;
-import site.archive.api.dto.archive.ArchiveUserIdRequestDto;
 import site.archive.api.resolver.annotation.RequestUser;
 import site.archive.domain.archive.ArchiveService;
 import site.archive.domain.user.info.UserInfo;
@@ -24,10 +22,10 @@ public class ArchiveControllerV2 {
     private final ArchiveService archiveService;
 
     @Operation(summary = "특정 유저 아카이브 리스트 조회")
-    @GetMapping
+    @GetMapping("/other")
     public ResponseEntity<ArchiveListResponseDto> archiveListView(@RequestUser UserInfo user,
-                                                                  @Validated @RequestBody ArchiveUserIdRequestDto archiveUserIdRequestDto) {
-        return ResponseEntity.ok(archiveService.getAllArchive(user, archiveUserIdRequestDto.getAuthorId()));
+                                                                  @RequestParam Long userId) {
+        return ResponseEntity.ok(archiveService.getAllArchive(user, userId));
     }
 
     @Operation(summary = "아카이브 상세 조회")
