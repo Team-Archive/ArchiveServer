1dcf0ae3011fe0d94b66439cecf72c806572b809
KimDoubleB <tree9295@gmail.com>
2022-06-17 23:08:29 +0900

    refactor(#9): ignore UserInfo class parameter at controller

--- src/main/java/site/archive/config/SwaggerConfig.java
+++ src/main/java/site/archive/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package site.archive.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import site.archive.domain.user.info.UserInfo;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -30,7 +31,8 @@ public class SwaggerConfig {
                    .build()
                    .apiInfo(apiInfo())
                    .securityContexts(List.of(securityContext()))
-                   .securitySchemes(List.of(apiKey()));
+                   .securitySchemes(List.of(apiKey()))
+                   .ignoredParameterTypes(UserInfo.class);
     }
 
     private ApiInfo apiInfo() {
