9dbdf6bbb4fd3a2b8703593278a8379997c1d268
KimDoubleB <tree9295@gmail.com>
2022-06-17 23:35:37 +0900

    refactor(#9): added login endpoint info method for swagger

--- src/main/java/site/archive/api/v1/AuthControllerV1.java
+++ src/main/java/site/archive/api/v1/AuthControllerV1.java
@@ -1,5 +1,6 @@
 package site.archive.api.v1;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import site.archive.api.command.LoginCommand;
+import site.archive.api.command.PasswordRegisterCommand;
 import site.archive.api.dto.user.OAuthRegisterRequestDto;
 import site.archive.domain.user.UserRegisterService;
 import site.archive.domain.user.info.UserInfo;
@@ -32,6 +35,17 @@ public class AuthControllerV1 {
     private final TokenProvider tokenProvider;
     private final HttpAuthTokenSupport tokenSupport;
 
+    @SuppressWarnings(value = "all")
+    @Operation(summary = "패스워드 유저 로그인")
+    @PostMapping("/login")
+    public void loginUser(@RequestBody LoginCommand loginCommand) {
+        /*
+        Execute password user login process by BodyCredentialAuthenticationFilter.
+        This is just a class to print endpoint at swagger.
+         */
+    }
+
+    @Operation(summary = "패스워드 유저 회원가입")
     @PostMapping("/register")
     public ResponseEntity<Void> registerUser(@RequestBody @Valid PasswordRegisterCommand command) {
         encryptPassword(command);
@@ -39,6 +53,7 @@ public class AuthControllerV1 {
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "소셜 로그인 유저 회원가입 및 로그인")
     @PostMapping("/social")
     public ResponseEntity<Void> registerOrLoginSocialUser(HttpServletResponse httpServletResponse,
                                                           @Validated @RequestBody OAuthRegisterRequestDto oAuthRegisterRequestDto) {
