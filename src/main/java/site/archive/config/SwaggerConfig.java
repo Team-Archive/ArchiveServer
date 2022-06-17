039f9b1aea75a3ec02e6a9467da4c8b86786f4ae
KimDoubleB <tree9295@gmail.com>
2022-06-18 00:21:36 +0900

    refactor(#9): set dynamically swagger url

--- src/main/java/site/archive/config/SwaggerConfig.java
+++ src/main/java/site/archive/config/SwaggerConfig.java
@@ -1,11 +1,16 @@
 package site.archive.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import site.archive.domain.user.info.UserInfo;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.web.OpenApiTransformationContext;
+import springfox.documentation.oas.web.WebMvcOpenApiTransformationFilter;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.AuthorizationScope;
@@ -14,12 +19,17 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
+import javax.servlet.http.HttpServletRequest;
+
 import java.util.List;
 
 @Configuration
-public class SwaggerConfig {
+public class SwaggerConfig implements WebMvcOpenApiTransformationFilter {
 
     private static final String AUTHORIZATION_HEADER = "Authorization";
+
+    @Value("${archive.server.url}")
+    private String baseUrl;
 
     @Bean
     public Docket api() {
@@ -33,6 +43,21 @@ public class SwaggerConfig {
                    .securityContexts(List.of(securityContext()))
                    .securitySchemes(List.of(apiKey()))
                    .ignoredParameterTypes(UserInfo.class);
+    }
+
+    @Override
+    public OpenAPI transform(OpenApiTransformationContext<HttpServletRequest> context) {
+        OpenAPI openApi = context.getSpecification();
+        Server server = new Server();
+        server.setDescription("Archive server");
+        server.setUrl(baseUrl);
+        openApi.setServers(List.of(server));
+        return openApi;
+    }
+
+    @Override
+    public boolean supports(DocumentationType documentationType) {
+        return documentationType.equals(DocumentationType.OAS_30);
     }
 
     private ApiInfo apiInfo() {
