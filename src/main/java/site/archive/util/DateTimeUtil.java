5c41a9945ebf4b47fa476231fd9b41526efa1b5e
KimDoubleB <tree9295@gmail.com>
2022-06-21 19:36:59 +0900

    feat: added datetime to milli method of Asia zone

--- src/main/java/site/archive/util/DateTimeUtil.java
+++ src/main/java/site/archive/util/DateTimeUtil.java
@@ -1,5 +1,8 @@
 package site.archive.util;
 
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class DateTimeUtil {
@@ -7,5 +10,12 @@ public class DateTimeUtil {
     public static final DateTimeFormatter YY_MM_DD_FORMATTER = DateTimeFormatter.ofPattern("yy/MM/dd");
     public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
+    public static final ZoneId ASIA_ZONE = ZoneId.of("Asia/Seoul");
+
     private DateTimeUtil() {}
+
+    public static ZonedDateTime fromMilli(long milli) {
+        return Instant.ofEpochMilli(milli).atZone(ASIA_ZONE);
+    }
+
 }
