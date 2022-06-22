9b8977ec0f79ea55cd89b161a1f2801b796ab098
KimDoubleB <tree9295@gmail.com>
2022-06-22 20:31:22 +0900

    refactor(#11): reuse soft-deleted row at Like entity

--- src/main/java/site/archive/domain/like/LikeService.java
+++ src/main/java/site/archive/domain/like/LikeService.java
@@ -3,6 +3,7 @@ package site.archive.domain.like;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import site.archive.domain.common.BaseTimeEntity;
 import site.archive.domain.like.entity.Like;
 import site.archive.exception.common.ResourceNotFoundException;
 
@@ -15,14 +16,16 @@ public class LikeService {
 
     @Transactional
     public void save(Long userId, Long archiveId) {
-        likeRepository.save(Like.of(userId, archiveId));
+        likeRepository.findByUserIdAndArchiveId(userId, archiveId)
+                      .ifPresentOrElse(BaseTimeEntity::deleteCancel,
+                                       () -> likeRepository.save(Like.of(userId, archiveId)));
     }
 
     @Transactional
     public void delete(Long userId, Long archiveId) {
-        var like = likeRepository.findByUserIdAndArchiveId(userId, archiveId)
-                                 .orElseThrow(() -> new ResourceNotFoundException("조건에 맞는 Like 데이터가 없습니다"));
-        likeRepository.delete(like);
+        likeRepository.findByUserIdAndArchiveId(userId, archiveId)
+                      .ifPresentOrElse(likeRepository::delete,
+                                       () -> {throw new ResourceNotFoundException("조건에 맞는 Like 데이터가 없습니다");});
     }
 
 }
