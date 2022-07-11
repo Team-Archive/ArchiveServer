e2f0f9f1f63211022401d03c3d3028ed047c1e9b
KimDoubleB <tree9295@gmail.com>
2022-07-11 22:45:43 +0900

    refactor(#14): QueryDSL method split

--- src/main/java/site/archive/domain/archive/ArchiveCustomRepositoryImpl.java
+++ src/main/java/site/archive/domain/archive/ArchiveCustomRepositoryImpl.java
@@ -1,6 +1,7 @@
 package site.archive.domain.archive;
 
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -19,50 +20,54 @@ public class ArchiveCustomRepositoryImpl implements ArchiveCustomRepository {
 
     @Override
     public List<Archive> findFirstPageOnlyPublic(ArchivePageable pageable, int pageElementSize) {
-        var firstPageWhere = addEmotionWhereWhenEmotionExist(archive.isPublic.eq(true),
-                                                             pageable.getEmotion());
-        var timeSortTypeDescOrderBy = pageable.getSortType().getOrderBy(archive);
-        var archiveIdDescOrderBy = archive.id.desc();
-        return jpaQueryFactory.selectFrom(archive)
-                              .innerJoin(archive.author).fetchJoin()
-                              .where(firstPageWhere)
-                              .orderBy(timeSortTypeDescOrderBy, archiveIdDescOrderBy)
-                              .limit(pageElementSize)
-                              .fetch();
+        var archiveQuery = baseArchiveSelectQuery(pageable);
+        var onlyPublicWhere = archive.isPublic.eq(true);
+        return whereEmotionIfExists(archiveQuery, pageable.getEmotion())
+                   .where(onlyPublicWhere)
+                   .limit(pageElementSize)
+                   .fetch();
     }
 
     @Override
     public List<Archive> findNextPageOnlyPublic(ArchivePageable pageable, int pageElementSize) {
-        var nextPageWhere = getNextPageWhere(pageable);
+        var archiveQuery = baseArchiveSelectQuery(pageable);
+        var onlyPublicWhere = archive.isPublic.eq(true);
+        return whereEmotionIfExists(archiveQuery, pageable.getEmotion())
+                   .where(onlyPublicWhere, getNextPageWhere(pageable))
+                   .limit(pageElementSize)
+                   .fetch();
+    }
+
+    /**
+     * 기본 Archive page select query
+     * - Join User table
+     * - order by TimeSortType, Archive id
+     */
+    private JPAQuery<Archive> baseArchiveSelectQuery(ArchivePageable pageable) {
         var timeSortTypeDescOrderBy = pageable.getSortType().getOrderBy(archive);
         var archiveIdDescOrderBy = archive.id.desc();
         return jpaQueryFactory.selectFrom(archive)
                               .innerJoin(archive.author).fetchJoin()
-                              .where(nextPageWhere)
-                              .orderBy(timeSortTypeDescOrderBy, archiveIdDescOrderBy)
-                              .limit(pageElementSize)
-                              .fetch();
+                              .orderBy(timeSortTypeDescOrderBy, archiveIdDescOrderBy);
+    }
+
+    /**
+     * Emotion이 있는 경우(Not null), where 조건 절을 추가한다.
+     */
+    private JPAQuery<Archive> whereEmotionIfExists(JPAQuery<Archive> jpaQuery,
+                                                   Emotion emotion) {
+        return emotion == null ? jpaQuery : jpaQuery.where(archive.emotion.eq(emotion));
     }
 
     /**
      * TimeSortType에 해당하는 필드의 값이 lastSeenArchiveDateMilli 값보다 작거나,
      * 같은 경우, archiveId에 해당하는 필드의 값이 lastSeenArchiveId 값보다 작아야 한다.
-     * 그리고 public Archive만 조회하며, Emotion 필터가 설정된 경우 주어진 Emotion의 archive만 조회한다.
      */
     private BooleanExpression getNextPageWhere(ArchivePageable pageable) {
         var timeSortTypeLtWhere = pageable.getSortType().getLtWhere(archive, pageable.getLastArchiveDateTime());
         var timeSortTypeEqWhere = pageable.getSortType().getEqWhere(archive, pageable.getLastArchiveDateTime());
         var archiveIdLtWhere = archive.id.lt(pageable.getLastArchiveId());
-        var timeSortTypeWhere = timeSortTypeLtWhere
-                                    .or(timeSortTypeEqWhere.and(archiveIdLtWhere));
-        var publicArchiveWhere = archive.isPublic.eq(true);
-        var where = timeSortTypeWhere.and(publicArchiveWhere);
-        return addEmotionWhereWhenEmotionExist(where, pageable.getEmotion());
-    }
-
-    private BooleanExpression addEmotionWhereWhenEmotionExist(BooleanExpression where,
-                                                              Emotion emotion) {
-        return emotion == null ? where : where.and(archive.emotion.eq(emotion));
+        return timeSortTypeLtWhere.or(timeSortTypeEqWhere.and(archiveIdLtWhere));
     }
 
 }
