0d98365c77596da5611f0447a15c636e3e9cd7d9
KimDoubleB <tree9295@gmail.com>
2022-07-17 19:37:24 +0900

    refactor: rename constant ASIA_ZONE to ASIA_SEOUL_ZONE

--- src/main/java/site/archive/util/DateTimeUtil.java
+++ src/main/java/site/archive/util/DateTimeUtil.java
@@ -10,12 +10,12 @@ public class DateTimeUtil {
     public static final DateTimeFormatter YY_MM_DD_FORMATTER = DateTimeFormatter.ofPattern("yy/MM/dd");
     public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-    public static final ZoneId ASIA_ZONE = ZoneId.of("Asia/Seoul");
+    public static final ZoneId ASIA_SEOUL_ZONE = ZoneId.of("Asia/Seoul");
 
     private DateTimeUtil() {}
 
     public static ZonedDateTime fromMilli(long milli) {
-        return Instant.ofEpochMilli(milli).atZone(ASIA_ZONE);
+        return Instant.ofEpochMilli(milli).atZone(ASIA_SEOUL_ZONE);
     }
 
 }
