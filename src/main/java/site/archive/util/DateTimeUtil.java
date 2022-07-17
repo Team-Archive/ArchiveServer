74e0b4d8a7a9e106ab714785e4a73ad934dafc2b
KimDoubleB <tree9295@gmail.com>
2022-07-18 00:47:25 +0900

    feat: first datetime calculate method of DateTimeUtil

--- src/main/java/site/archive/util/DateTimeUtil.java
+++ src/main/java/site/archive/util/DateTimeUtil.java
@@ -1,10 +1,17 @@
 package site.archive.util;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.Clock;
 import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.YearMonth;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
+@Slf4j
 public class DateTimeUtil {
 
     public static final DateTimeFormatter YY_MM_DD_FORMATTER = DateTimeFormatter.ofPattern("yy/MM/dd");
@@ -12,10 +19,25 @@ public class DateTimeUtil {
 
     public static final ZoneId ASIA_SEOUL_ZONE = ZoneId.of("Asia/Seoul");
 
+    private static Clock clock = Clock.system(ASIA_SEOUL_ZONE);
+
     private DateTimeUtil() {}
+
+    public static void changeClock(LocalDate date) {
+        log.warn("Change clock of DateTimeUtil. It may effect now() methods of DateTimeUtil.");
+        var dateTime = date.atStartOfDay();
+        var zoneOffset = ASIA_SEOUL_ZONE.getRules().getOffset(dateTime);
+        clock = Clock.fixed(dateTime.atOffset(zoneOffset).toInstant(), zoneOffset);
+    }
 
     public static ZonedDateTime fromMilli(long milli) {
         return Instant.ofEpochMilli(milli).atZone(ASIA_SEOUL_ZONE);
+    }
+
+    public static LocalDateTime firstDateTimeOfMonth() {
+        return YearMonth.now(clock)
+                        .atDay(1)
+                        .atTime(0, 0);
     }
 
 }
