361ed2041b2b661340e0673d8e5e458f49cd0eb0
KimDoubleB <tree9295@gmail.com>
2022-10-13 08:30:58 +0900

    feat: create banner logic, endpoints

--- archive-application/src/main/java/site/archive/service/banner/BannerService.java
+++ archive-application/src/main/java/site/archive/service/banner/BannerService.java
@@ -2,11 +2,15 @@ package site.archive.service.banner;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import site.archive.domain.banner.Banner;
 import site.archive.domain.banner.BannerRepository;
+import site.archive.domain.banner.BannerType;
 import site.archive.dto.v2.BannerListResponseDto;
 import site.archive.dto.v2.BannerResponseDto;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class BannerService {
 
@@ -19,6 +23,12 @@ public class BannerService {
                                                                                   banner.getMainContent()))
                                              .toList();
         return new BannerListResponseDto(bannerResponse);
+    }
+
+    @Transactional
+    public BannerResponseDto createBanner(String summaryImageUri, String mainContent, BannerType type) {
+        var banner = bannerRepository.save(new Banner(summaryImageUri, mainContent, type));
+        return BannerResponseDto.from(banner);
     }
 
 }
--- archive-web/src/main/java/site/archive/web/api/v2/BannerControllerV2.java
+++ archive-web/src/main/java/site/archive/web/api/v2/BannerControllerV2.java
@@ -2,12 +2,21 @@ package site.archive.web.api.v2;
 
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import site.archive.domain.banner.BannerType;
 import site.archive.dto.v2.BannerListResponseDto;
+import site.archive.service.archive.ArchiveImageService;
 import site.archive.service.banner.BannerService;
+
+import static site.archive.service.archive.ArchiveImageService.BANNER_MAIN_IMAGE_DIRECTORY;
+import static site.archive.service.archive.ArchiveImageService.BANNER_SUMMARY_IMAGE_DIRECTORY;
 
 @RestController
 @RequestMapping("/api/v2/banner")
@@ -15,11 +24,40 @@ import site.archive.service.banner.BannerService;
 public class BannerControllerV2 {
 
     private final BannerService bannerService;
+    private final ArchiveImageService imageService;
 
     @Operation(summary = "배너 조회")
     @GetMapping
     public ResponseEntity<BannerListResponseDto> archiveCommunityBannerView() {
         return ResponseEntity.ok(bannerService.getAllBanner());
+    }
+
+    @Operation(summary = "배너 생성 (업로드) - 이미지 타입")
+    @PostMapping(path = "/type/image",
+                 consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+                 produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> createArchiveCommunityBanner(@RequestParam MultipartFile summaryImage,
+                                                             @RequestParam MultipartFile mainImage) {
+        var summaryImageUri = imageUploadAndGetUri(BANNER_SUMMARY_IMAGE_DIRECTORY, summaryImage);
+        var mainImageUri = imageUploadAndGetUri(BANNER_MAIN_IMAGE_DIRECTORY, mainImage);
+        bannerService.createBanner(summaryImageUri, mainImageUri, BannerType.IMAGE);
+        return ResponseEntity.noContent().build();
+    }
+
+    @Operation(summary = "배너 생성 (업로드) - URL 타입")
+    @PostMapping(path = "/type/url",
+                 consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+                 produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> createArchiveCommunityBanner(@RequestParam MultipartFile summaryImage,
+                                                             @RequestParam String mainContentUrl) {
+        var summaryImageUri = imageUploadAndGetUri(BANNER_SUMMARY_IMAGE_DIRECTORY, summaryImage);
+        bannerService.createBanner(summaryImageUri, mainContentUrl, BannerType.URL);
+        return ResponseEntity.noContent().build();
+    }
+
+    private String imageUploadAndGetUri(String directory, MultipartFile imageFile) {
+        imageService.verifyImageFile(imageFile);
+        return imageService.upload(directory, imageFile);
     }
 
 }
