9a44a851cffeb50ec7b48ee539bd8bc3bcd94653
KimDoubleB <tree9295@gmail.com>
2022-10-13 17:00:38 +0900

    feat: added archive report slack alarm

--- archive-application/src/main/java/site/archive/service/message/MessagingService.java
+++ archive-application/src/main/java/site/archive/service/message/MessagingService.java
@@ -1,9 +1,12 @@
 package site.archive.service.message;
 
+import site.archive.dto.v1.archive.ArchiveDto;
 import site.archive.dto.v1.user.BaseUserDto;
 
 public interface MessagingService {
 
     void sendUserRegisterMessage(BaseUserDto user, String registerType);
+
+    void sendArchiveReportMessage(String userEmail, String reportReason, ArchiveDto archive);
 
 }
