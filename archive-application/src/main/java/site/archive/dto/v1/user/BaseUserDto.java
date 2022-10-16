05bdb2b57903b7ae606d89da7d481ae6501194a0
KimDoubleB <tree9295@gmail.com>
2022-10-16 22:00:11 +0900

    feat: added user role field to BaseUserDto

--- archive-application/src/main/java/site/archive/dto/v1/user/BaseUserDto.java
+++ archive-application/src/main/java/site/archive/dto/v1/user/BaseUserDto.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import site.archive.common.DateTimeUtil;
 import site.archive.domain.user.BaseUser;
+import site.archive.domain.user.UserRole;
 
 import java.time.LocalDateTime;
 
@@ -13,12 +14,14 @@ public class BaseUserDto {
 
     private final Long userId;
     private final String mailAddress;
+    private final UserRole userRole;
     private final LocalDateTime createdAt;
     private final String profileImage;
 
     public static BaseUserDto from(BaseUser baseUser) {
         return new BaseUserDto(baseUser.getId(),
                                baseUser.getMailAddress(),
+                               baseUser.getRole(),
                                baseUser.getCreatedAt(),
                                baseUser.getProfileImage());
     }
