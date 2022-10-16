1f36e2daa1f9fe0c6e9b4d0b87c35bb360ce8a2e
KimDoubleB <tree9295@gmail.com>
2022-10-16 22:42:20 +0900

    feat: added user type field, update method to BaseUserDto

--- archive-application/src/main/java/site/archive/dto/v1/user/BaseUserDto.java
+++ archive-application/src/main/java/site/archive/dto/v1/user/BaseUserDto.java
@@ -1,5 +1,6 @@
 package site.archive.dto.v1.user;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import site.archive.common.DateTimeUtil;
@@ -8,6 +9,7 @@ import site.archive.domain.user.UserRole;
 
 import java.time.LocalDateTime;
 
+@AllArgsConstructor
 @RequiredArgsConstructor
 @Getter
 public class BaseUserDto {
@@ -18,16 +20,24 @@ public class BaseUserDto {
     private final LocalDateTime createdAt;
     private final String profileImage;
 
+    // mutable field
+    private String userType;
+
     public static BaseUserDto from(BaseUser baseUser) {
         return new BaseUserDto(baseUser.getId(),
                                baseUser.getMailAddress(),
                                baseUser.getRole(),
                                baseUser.getCreatedAt(),
-                               baseUser.getProfileImage());
+                               baseUser.getProfileImage(),
+                               baseUser.getUserType());
     }
 
     public String getCreatedAt() {
         return DateTimeUtil.DATE_TIME_FORMATTER.format(this.createdAt);
+    }
+
+    public void updateSpecificUserType(String userType) {
+        this.userType = userType;
     }
 
 }
