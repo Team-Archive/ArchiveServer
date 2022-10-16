ad24d1029b735bf8983c35c3ab2e8f0db4595dd7
KimDoubleB <tree9295@gmail.com>
2022-10-16 22:43:52 +0900

    feat: added endpoint, logic for user profile inquiry

--- archive-application/src/main/java/site/archive/service/user/UserService.java
+++ archive-application/src/main/java/site/archive/service/user/UserService.java
@@ -5,6 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import site.archive.common.exception.common.ResourceNotFoundException;
+import site.archive.domain.user.OAuthUser;
+import site.archive.domain.user.OAuthUserRepository;
+import site.archive.domain.user.PasswordUserRepository;
 import site.archive.domain.user.UserRepository;
 import site.archive.dto.v1.user.BaseUserDto;
 
@@ -15,11 +18,19 @@ import site.archive.dto.v1.user.BaseUserDto;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordUserRepository passwordUserRepository;
+    private final OAuthUserRepository oAuthUserRepository;
 
     public BaseUserDto findUserById(long userId) {
         return userRepository.findById(userId)
                              .map(BaseUserDto::from)
                              .orElseThrow(() -> new ResourceNotFoundException("아이디에 해당하는 유저가 존재하지 않습니다."));
+    }
+
+    public BaseUserDto findSpecificUserById(long userId) {
+        var user = findUserById(userId);
+        updateUserTypeWithProviderWhenOAuthUser(userId, user);
+        return user;
     }
 
     public boolean existsEmail(String email) {
@@ -29,6 +40,15 @@ public class UserService {
     @Transactional
     public void deleteUser(long userId) {
         userRepository.deleteById(userId);
+    }
+
+    private void updateUserTypeWithProviderWhenOAuthUser(long userId, BaseUserDto user) {
+        if (OAuthUser.OAUTH_TYPE.equals(user.getUserType())) {
+            var oAuthType = oAuthUserRepository.findById(userId)
+                                               .map(oAuthUser -> oAuthUser.getOAuthProvider().toString())
+                                               .orElseThrow(() -> new ResourceNotFoundException("아이디에 해당하는 OAuth 유저가 존재하지 않습니다."));
+            user.updateSpecificUserType(oAuthType);
+        }
     }
 
 }
--- archive-web/src/main/java/site/archive/web/api/v2/UserProfileControllerV2.java
+++ archive-web/src/main/java/site/archive/web/api/v2/UserProfileControllerV2.java
@@ -5,12 +5,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import site.archive.domain.user.UserInfo;
+import site.archive.dto.v1.user.BaseUserDto;
 import site.archive.service.archive.ArchiveImageService;
 import site.archive.service.user.UserProfileImageService;
 import site.archive.service.user.UserService;
@@ -26,6 +28,12 @@ public class UserProfileControllerV2 {
     private final UserService userService;
     private final UserProfileImageService userProfileImageService;
     private final ArchiveImageService imageService;
+
+    @Operation(summary = "프로필 정보 조회")
+    @GetMapping
+    public ResponseEntity<BaseUserDto> getUserProfileInfo(@RequestUser UserInfo user) {
+        return ResponseEntity.ok(userService.findSpecificUserById(user.getUserId()));
+    }
 
     @Operation(summary = "프로필 이미지 업로드 및 업데이트")
     @PostMapping(path = "/image/upload",
