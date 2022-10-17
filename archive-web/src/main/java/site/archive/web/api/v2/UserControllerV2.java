65ccfb3c4b5dabacf45be831b4f69b92b5c17f6f
KimDoubleB <tree9295@gmail.com>
2022-10-17 11:36:55 +0900

    feat: unify request param value (email/nickname duplicate check)

--- archive-web/src/main/java/site/archive/web/api/v2/UserControllerV2.java
+++ archive-web/src/main/java/site/archive/web/api/v2/UserControllerV2.java
@@ -25,14 +25,14 @@ public class UserControllerV2 {
 
     @Operation(summary = "[NoAuth] 이메일 중복 검사")
     @GetMapping("/duplicate/email")
-    public ResponseEntity<EmailDuplicateResponseDto> checkDuplicatedEmail(@RequestParam String email) {
+    public ResponseEntity<EmailDuplicateResponseDto> checkDuplicatedEmail(@RequestParam(value = "value") String email) {
         var emailDuplicateResponseDto = new EmailDuplicateResponseDto(userService.existsEmail(email));
         return ResponseEntity.ok(emailDuplicateResponseDto);
     }
 
     @Operation(summary = "[NoAuth] 닉네임 중복 검사")
     @GetMapping("/duplicate/nickname")
-    public ResponseEntity<NicknameDuplicateResponseDto> checkDuplicatedNickname(@RequestParam String nickname) {
+    public ResponseEntity<NicknameDuplicateResponseDto> checkDuplicatedNickname(@RequestParam(value = "value") String nickname) {
         var nicknameDuplicateResponseDto = new NicknameDuplicateResponseDto(userService.existsNickname(nickname));
         return ResponseEntity.ok(nicknameDuplicateResponseDto);
     }
