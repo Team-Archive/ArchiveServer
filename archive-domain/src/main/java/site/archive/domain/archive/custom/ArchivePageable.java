67f62c04a0b42af13e236ebfbadbeff79740c572
KimDoubleB <tree9295@gmail.com>
2022-12-13 00:28:43 +0900

    feat: set cache for archive communities api

--- archive-domain/src/main/java/site/archive/domain/archive/custom/ArchivePageable.java
+++ archive-domain/src/main/java/site/archive/domain/archive/custom/ArchivePageable.java
@@ -1,11 +1,13 @@
 package site.archive.domain.archive.custom;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import site.archive.domain.archive.Emotion;
 
-@AllArgsConstructor
 @Getter
+@AllArgsConstructor
+@EqualsAndHashCode
 public class ArchivePageable {
 
     private ArchiveCommunityTimeSortType sortType;
