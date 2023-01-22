fa021b1e5b4681854a60dd886eac9fc66b21f334
KimDoubleB <tree9295@gmail.com>
2023-01-22 16:59:55 +0900

    fix: ignore jwt filter at actuator path

--- archive-web/src/main/java/site/archive/web/config/security/token/jwt/JwtTokenPersistFilter.java
+++ archive-web/src/main/java/site/archive/web/config/security/token/jwt/JwtTokenPersistFilter.java
@@ -1,5 +1,10 @@
 package site.archive.web.config.security.token.jwt;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -7,16 +12,13 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import site.archive.web.config.security.token.HttpAuthTokenSupport;
 import site.archive.web.config.security.token.TokenProvider;
 
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
 
 @RequiredArgsConstructor
 @Slf4j
 public class JwtTokenPersistFilter extends OncePerRequestFilter {
+
+    private static final String MONITORING_PATH_PREFIX = "/actuator";
 
     private final HttpAuthTokenSupport httpTokenExtractor;
     private final TokenProvider tokenProvider;
@@ -24,12 +26,14 @@ public class JwtTokenPersistFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
         throws ServletException, IOException {
-        try {
-            var tokenStr = httpTokenExtractor.extractToken(request);
-            var authToken = tokenProvider.parseUserInfoFromToken(tokenStr);
-            SecurityContextHolder.getContext().setAuthentication(new JwtAuthenticationToken(authToken));
-        } catch (Exception e) {
-            SecurityContextHolder.clearContext();
+        if (!request.getRequestURI().startsWith(MONITORING_PATH_PREFIX)) {
+            try {
+                var tokenStr = httpTokenExtractor.extractToken(request);
+                var authToken = tokenProvider.parseUserInfoFromToken(tokenStr);
+                SecurityContextHolder.getContext().setAuthentication(new JwtAuthenticationToken(authToken));
+            } catch (Exception e) {
+                SecurityContextHolder.clearContext();
+            }
         }
         filterChain.doFilter(request, response);
     }
