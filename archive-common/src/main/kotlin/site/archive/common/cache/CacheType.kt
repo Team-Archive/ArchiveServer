4f780af2b92087952071b399ab9797ff8d6d6601
KimDoubleB <tree9295@gmail.com>
2023-01-24 16:35:23 +0900

    refactor: rename banner expireAfterWrite variable name
    - added SECONDS suffix

--- archive-common/src/main/kotlin/site/archive/common/cache/CacheType.kt
+++ archive-common/src/main/kotlin/site/archive/common/cache/CacheType.kt
@@ -2,18 +2,18 @@ package site.archive.common.cache
 
 enum class CacheType(val key: String, val expireAfterWrite: Int, val maximumSize: Int) {
 
-    BANNERS(CacheInfo.BANNERS, CacheInfo.BANNERS_GET_EXPIRE_TIME, CacheInfo.BANNERS_CACHE_SIZE),
-    ARCHIVE_COMMUNITIES(CacheInfo.ARCHIVE_COMMUNITIES, CacheInfo.ARCHIVE_COMMUNITIES_GET_EXPIRE_TIME, CacheInfo.ARCHIVE_COMMUNITIES_CACHE_SIZE)
+    BANNERS(CacheInfo.BANNERS, CacheInfo.BANNERS_GET_EXPIRE_SECONDS, CacheInfo.BANNERS_CACHE_SIZE),
+    ARCHIVE_COMMUNITIES(CacheInfo.ARCHIVE_COMMUNITIES, CacheInfo.ARCHIVE_COMMUNITIES_GET_EXPIRE_SECONDS, CacheInfo.ARCHIVE_COMMUNITIES_CACHE_SIZE)
     ;
 
     class CacheInfo {
         companion object {
             const val BANNERS: String = "banners"
-            const val BANNERS_GET_EXPIRE_TIME: Int = 3_600
+            const val BANNERS_GET_EXPIRE_SECONDS: Int = 3_600
             const val BANNERS_CACHE_SIZE: Int = 1
 
             const val ARCHIVE_COMMUNITIES: String = "archive-communities"
-            const val ARCHIVE_COMMUNITIES_GET_EXPIRE_TIME: Int = 30
+            const val ARCHIVE_COMMUNITIES_GET_EXPIRE_SECONDS: Int = 30
             const val ARCHIVE_COMMUNITIES_CACHE_SIZE: Int = 200
         }
     }
